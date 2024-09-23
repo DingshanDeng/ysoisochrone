012d8dcb48ef43f9766493f64a80400e21d79148
Dingshan Deng <dingshandeng@gmail.com>
2024-09-23 14:05:27 -0700

    rename the tutorials

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ysoisochrone',
-    version='0.2.0.alpha',
+    version='0.2.1.alpha',
     description='Python package handles the young-stellar-objects isochrones, and one primary goal is to derive the stellar mass and ages from the isochrones.',
     author='Dingshan Deng',
     author_email='dingshandeng@arizona.edu',
