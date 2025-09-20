a22a03eced633a2f5c191cfdee9127af25cff434
Dingshan Deng <dingshandeng@gmail.com>
2025-09-20 15:57:42 -0700

    update the doc page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,9 @@ sys.path.insert(0, os.path.abspath('..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ysoisochrone'
-copyright = '2024, Dingshan Deng'
+copyright = '2024-2025, Dingshan Deng'
 author = 'Dingshan Deng'
-release = '0.3.2.beta'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
